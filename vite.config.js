c3dde178012ccd3563374a01aa48f1424fbd065a
nadgesachin <nadgesachin644@gmail.com>
2024-09-04 13:28:50 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // Port for the development server
+    host: '0.0.0.0', // Listen on all network interfaces
+  },
+  preview: {
+    port: 3000, // Port for the preview server
+    host: '0.0.0.0', // Listen on all network interfaces
+  },
+});
