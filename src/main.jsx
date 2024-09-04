5eb120c9fefd760cc08a840c70bf20bc1451079c
nadgesachin <nadgesachin644@gmail.com>
2024-09-04 13:10:08 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import ProductGrid from './components/Product/ProductGrid.jsx'
 import ProductDetails from './components/Product/ProductDetails.jsx'
 import ShoppingCart from './components/Product/ShoppingCart.jsx'
 import CheckoutProcess from './components/Product/CheckoutProcess.jsx'
 import UserAccountDashboard from './components/User/UserAccountDashboard.jsx'
-import About from '../../mywebsite/src/components/About/About.jsx'
-import Contact from '../../mywebsite/src/components/Contact/Contact.jsx'
+
 import NotFound from './components/NotFound/NotFound.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import ProductGrid from './components/Product/ProductGrid.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
         <Route path="/" element={<ProductGrid />} />
         <Route path='/about' element={<About />} />
+        <Route path='/shop' element={<ProductGrid />} />
         <Route path='/contact' element={<Contact />} />
         <Route path="/product/:id" element={<ProductDetails />} />
         <Route path="/cart" element={<ShoppingCart />} />
